c5fcfee31e98781bbe31d6aa6fced3b9b0581143
Abhya Anand <abhyaanand2004@gmail.com>
2024-07-22 20:15:43 +0530

    de-structure

---
+++ 02_basics/05_objects3.js
@@ -0,0 +1,41 @@
+const course = {
+    courseName: "JavaScript",
+    price: "999",
+    courseInstructor: "Hitesh"
+}
+
+// another way to access (DE-STRUCTURE)
+const {courseInstructor: instructor} = course 
+console.log(instructor);
+
+//REACT
+// const navbar = ({company}) => {
+
+// }
+
+// navbar(company = "google")
+
+//API call in json
+
+//JSON - obj format
+// {
+//     "name": "Abhya",
+//     "courseName": "JavaScript",
+//     "price":  "free"
+// }
+
+//array format - multiple obj in array
+[
+    {
+
+    },
+    {
+
+    },
+    {
+
+    }
+]
+//randdom user me API
+//json formatter
+ 
