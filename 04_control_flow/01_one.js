6f4afd558470d3c96aaee941a24b2aaea89e0ba3
Abhya Anand <abhyaanand2004@gmail.com>
2024-07-24 10:30:42 +0530

    if else

---
+++ 04_control_flow/01_one.js
@@ -0,0 +1,43 @@
+// IF
+
+/*
+if(2  == "2") {
+    console.log("executed"); 
+}
+
+// === check datatype also
+if(2  === "2") {
+    console.log("executed");
+}
+*/
+
+// block scope
+/*
+const score = 200
+if(score > 100) {
+    const power = "fly"
+    console.log(`user power: ${power}`);
+}
+
+console.log(`user power: ${power}`); // not accessed outside (if var can be accessed)
+*/
+
+//short hand notation
+const balance = 1000
+// if(balance>500) console.log("test"), console.log("test2");
+
+/*
+if (condition) {
+    
+}
+else if (condition) {
+    
+}
+else{
+
+}
+*/
+
+
+// && ---> and
+// || ---> or
