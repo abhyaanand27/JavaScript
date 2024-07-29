15537bee5b49d417a6cb8cb76fad3914d411e2f6
Abhya Anand <abhyaanand2004@gmail.com>
2024-07-29 19:54:13 +0530

    reduce

--- 05_iteration/06_reduce.js
+++ 05_iteration/06_reduce.js
@@ -7,8 +7,34 @@ const arr = [1, 2, 3, 4, 5]
 
 // console.log(sumWithInitial);
 
-const sum = arr.reduce( function (acc, currVal) {
-    return acc + currVal
-}, 0)
+// const sum = arr.reduce( function (acc, currVal) {
+//     console.log(`acc: ${acc} and currVal: ${currVal}`);
+//     return acc + currVal
+// }, 0)
 
-console.log(sum);
+const sum = arr.reduce( (acc, curr) => acc+curr, 0)
+
+// console.log(sum);
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "python course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    }
+]
+
+const priceToPay = shoppingCart.reduce( (acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
